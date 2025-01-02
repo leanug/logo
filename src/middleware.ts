b8d3be108184a95ca97070ed3264e3bb3a511852
Leandro Ubilla <leandroubilla@protonmail.com>
2025-01-02 15:06:46 -0300

    Install react hot toast

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,12 @@ import type { NextRequest } from "next/server"
  
 export async function middleware(req: NextRequest) {
   // 3. Decrypt the session from the cookie
-  const encryptedToken = req.cookies.get("next-auth.session-token")?.value || "";
+  const encryptedToken = req.cookies.get("next-auth.session-token")?.value || ""
   // Your existing logic for auth check can be added here
 
   // If there's no token, redirect to login page
   if (!encryptedToken) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
+    return NextResponse.redirect(new URL("/auth/login", req.url))
   }
 }
 
